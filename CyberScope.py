6154193a986d1ef4fc9b9f236a34f648541f4fba
assil-barhoumi <assil.barhoumi@isimg.tn>
2025-04-11 15:48:08 +0000

    Display general dataset info and summary statistics

--- CyberScope.py
+++ CyberScope.py
@@ -1,7 +1,9 @@
+from google.colab import drive
 import os
 import pandas as pd
 
-file_path = "/Users/macbook/ML/pfa/CyberScope/cyber_data.csv"
+drive.mount('/content/drive', force_remount=True)
+file_path = "/content/drive/MyDrive/pfa/cyber_data.csv"
 
 if not os.path.exists(file_path):
     print(f"ERROR: The file {file_path} does not exist. Please check the path!")
@@ -10,3 +12,15 @@ else:
     df = pd.read_csv(file_path)
     print("Preview of the first rows:")
     print(df.head())
+
+# General dataset info & summary statistics
+print("
+ Info about the dataset :")
+df.info()
+df.describe(include='all')
+
+#clean column names
+df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_", regex=False)
+print("Cleaned column names:")
+print(df.columns.tolist())
+
